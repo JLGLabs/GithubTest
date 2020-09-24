dc4f733230a0097b25c393a165e2e3886fd91281
JLGLabs <71471134+JLGLabs@users.noreply.github.com>
2020-09-24 14:14:00 -0400

    Add files via upload

---
+++ GithubTest/arrays.rb
@@ -0,0 +1,13 @@
+
+#friends = Array.new
+#friends[0] = "James"
+#friends[5] = "Jody"
+#puts friends
+
+
+friends = Array["Kevin", "Karen", "Oscar", "Jody"]
+puts friends.length() #length of array
+puts friends.include? "Jody" # is string included in array
+puts friends.reverse
+
+#This is a commit attempt for array.rb 9/24/20 01:07:00 PM ET
